7359979b79917785d402ead865c3a20973501413
davidmorck <david.morck@abbindustrigymnasium.se>
2018-11-27 15:21:46 +0100

    Andra slider ändrad

--- LAMPA/App/myapp/app/components/screen1/Component3.js
+++ LAMPA/App/myapp/app/components/screen1/Component3.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+
 import { 
 	StyleSheet,
 	View,
@@ -6,36 +8,63 @@ import {
 	Slider,
 } from 'react-native';
 
-
-
 export default class Component3 extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-		  value: 50,
+			Temp: 50,
 		};
-	  }
-	
-	  change(value) {
+		}
+		
+		componentDidMount(){
+			let self = this;
+			fetch("http://iot.abbindustrigymnasium.se:3001/grupp7/404",{
+				
+			method: "GET"
+			
+		}).then((response) => response.json()).then((responseJSON) =>
+		{
+			console.log(responseJSON);
+
+			var resultat = responseJSON;
+			console.log(resultat);
+			if (message = "Light7"){
+				if(responseJSON.length != 0 ){
+					self.setState({
+						Temp: resultat.temp
+					})
+				}
+				else
+				alert("Not found")
+				console.log(this.state)
+			}
+		}
+	)
+
+		}
+	  change(Temp) {
 		this.setState(() => {
 		  return {
-			value: parseFloat(value),
+			Temp: parseFloat(Temp),
 		  };
 		});
 	  }
 	
 	  render() {
-		const {value} = this.state;
+		const {Temp} = this.state;
 		return (
 		  <View style={styles.container}>
-			<Text style={styles.text}>{String(value)}</Text>
-			<Slider
+			<Text style={styles.text}>{this.state.Temp}</Text>
+			<View> 
+			<Slider style={styles.containernew}
 			  step={1}
 			  maximumValue={100}
 			  onValueChange={this.change.bind(this)}
-			  value={value}
+			  value={Temp}
 			/>
+
+			</View>
 		  </View>
 		);
 	  }
@@ -47,7 +76,14 @@ export default class Component3 extends React.Component {
 		flex: 1,
 		flexDirection: 'column',
 		justifyContent: 'center',
-	  },
+		},
+		containernew: {
+			padding: 25,
+			paddingTop: 5,
+			flex: 1,
+			height: 25,
+			justifyContent: 'center',
+			},
 	  text: {
 		fontSize: 50,
 		textAlign: 'center',
